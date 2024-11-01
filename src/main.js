2063e556b88fc446ceac40e64d68f30db2b9f858
Kostenko Andrey <andrey@MacBook-Pro-Kostenko.local>
2024-11-01 18:48:26 -0300

    final

--- src/main.js
+++ src/main.js
@@ -1,70 +1,61 @@
-`use strict`
+'use strict';
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { createMarkup, setupImageLoadHandlers } from "./js/render-functions";
+import { fetchData } from "./js/pixabay-api";
+
+
 const form = document.querySelector(".form");
-const API_KEY = "46815109-801c3b1dec2424a02adace61a";
+const container = document.querySelector(".gallery");
+let lightbox = null;
 
+form.addEventListener("submit", handleSearch);
 
-form.addEventListener("submit", hundleSearch);
-const BASE_URL = "https://pixabay.com/api/";
-const container = document.querySelector(".loader")
+function handleSearch(event) {
+    event.preventDefault();
+    const { textRow } = event.target.elements;
 
-function hundleSearch (event){
-event.preventDefault();
-const {button, textRow} = event.target.elements;
+    if (textRow.value.trim() === "") {
+        iziToast.warning({
+            title: 'Caution',
+            message: "The search field cannot be empty.",
+            color: "red"
+        });
+        return;
+    }
 
-if(textRow.value.trim() === ""){
-    return;
-}
+    fetchData(textRow.value)
+        .then(data => {
+            if (data.hits.length === 0) {
+                iziToast.warning({
+                    title: 'Caution',
+                    message: '"Sorry, there are no images matching your search query. Please try again!"',
+                    color: "yellow"
+                });
+            } else {
+                container.innerHTML = createMarkup(data.hits);
+                
+                setupImageLoadHandlers(container);
 
-fetchData(textRow.value)
-.then(data => {
-     console.log("data:", data);
-    container.innerHTML = (createMarkup(data.hits));
-})
-.catch(error => console.log(error));
-
+                if (lightbox) {
+                    lightbox.refresh();
+                } else {
+                    lightbox = new SimpleLightbox('.gallery .galleries a', {
+                        captionsData: 'alt',
+                        captionDelay: 250,
+                    });
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Fetch error:', error); 
+        });
+        
     form.reset();
-};
-
-
-function fetchData(textRow){
-    const params = new URLSearchParams ({
-        key: API_KEY,
-        q: textRow,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true,
-    });
-
-    return fetch(`${BASE_URL}?${params}`)
-    .then(response => {
-        if(!response.ok) {
-           throw new Error;
-        }
-        return response.json();
-    });  
-};
-
-
-function createMarkup(array){
-return array.map(( { webformatURL: smallPic, largeImageURL: largePic, tags: alt, likes = 0, views = 0, comments = "", downloads = 0 } ) => 
-      `<div class="gallery">
-    <a href="${largePic}"><img src="${smallPic}" alt="${alt}"/></a>
-    <div><p>Likes</p>
-    <span>${likes}</span></div>
-    <div><p>Views</p>
-    <span>${views}</span></div>
-    <div><p>Comments</p>
-    <span>${comments}</span></div>
-    <div><p>Downloads</p>
-    <span>${downloads}</span></div>
-</div>`
-)
-.join("");
 }
+
 
